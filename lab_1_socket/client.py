0c1bb6dc249ba53d3d9d8a8e625590f35d693060
skhortiuk <khortiukserhii@ukr.net>
2019-10-07 13:59:52 +0300

    Added lab_1

---
+++ lab_1_socket/client.py
@@ -0,0 +1,45 @@
+import select
+import socket
+import sys
+
+HOST = '192.168.1.8'
+PORT = 8001
+
+
+def prompt():
+    sys.stdout.write('-> ')
+    sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+
+    try:
+        s.connect((HOST, PORT))
+    except Exception:
+        sys.exit()
+
+    while 1:
+        socket_list = [sys.stdin, s]
+
+        read_sockets, write_sockets, error_sockets = select.select(
+            socket_list,
+            [],
+            []
+        )
+
+        for sock in read_sockets:
+            if sock == s:
+                data = sock.recv(4096).decode()
+                if not data:
+                    sys.exit()
+                else:
+                    sys.stdout.write(f"\r{data}")
+                    prompt()
+
+            else:
+
+                msg = sys.stdin.readline()
+                s.send(msg.encode())
+                prompt()
