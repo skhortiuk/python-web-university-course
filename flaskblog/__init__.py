f0c9654ed99049b216101f8f8902cecf74c6abb2
skhort <khortiukserhii@ukr.net>
2020-01-22 23:42:54 +0200

    Stable version

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,8 +1,33 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_admin import Admin
+import os
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "asdqwdw1231231v1utc"
-
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,
+                                                                    'site.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+import flaskblog.forms as views
+
+admin = Admin(app)
+
+from flask_admin.contrib.sqla import ModelView
+
+admin.add_view(views.UserAdminView(views.User, db.session))
+
 from flaskblog import routes
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,19 +1,77 @@
 from datetime import datetime
+from flaskblog import db, login_manager, bcrypt
+from flask_login import UserMixin
+from hashlib import md5
+from wtforms import BooleanField, widgets, TextAreaField
 
-from flaskblog import db
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class User(db.Model):
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer,
+                               db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer,
+                               db.ForeignKey('user.id'))
+                     )
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False,
-                           default="default.png")
+                           default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship("Post", backref="author", lazy=True)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    admin = db.Column(db.Boolean())
+    notes = db.Column(db.UnicodeText)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+    def __init__(self, username, email, password, notes='', admin=False):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.admin = admin
+        self.notes = notes
+
+    def is_admin(self):
+        return self.admin
 
     def __repr__(self):
-        return f"User '{self.username}', '{self.email}', '{self.image_file}'"
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
 
 class Post(db.Model):
@@ -22,7 +80,17 @@ class Post(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False,
                             default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Post '{self.title}', '{self.date_posted}'"
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+class CKTextAreaWidget(widgets.TextArea):
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault('class_', 'ckeditor')
+        return super(CKTextAreaWidget, self).__call__(field, **kwargs)
+
+
+class CKTextAreaField(TextAreaField):
+    widget = CKTextAreaWidget()
